bca50e10ce00c67ba0a500065d024c257efc1643
Roman Sharkov <roman.sharkov@qbeon.com>
2018-11-29 13:38:51 +0100

    Add test

---
+++ test_test.go
@@ -0,0 +1,42 @@
+package gorilla_test
+
+import (
+	"context"
+	"testing"
+
+	webwire "github.com/qbeon/webwire-go"
+	wwrgorilla "github.com/qbeon/webwire-go-gorilla"
+	"github.com/stretchr/testify/require"
+)
+
+type TestSrvImpl struct{}
+
+func (tsi *TestSrvImpl) OnClientConnected(_ webwire.Connection)             {}
+func (tsi *TestSrvImpl) OnClientDisconnected(_ webwire.Connection, _ error) {}
+func (tsi *TestSrvImpl) OnSignal(
+	_ context.Context,
+	_ webwire.Connection,
+	_ webwire.Message,
+) {
+}
+func (tsi *TestSrvImpl) OnRequest(
+	_ context.Context,
+	_ webwire.Connection,
+	_ webwire.Message,
+) (payload webwire.Payload, err error) {
+	return webwire.Payload{}, nil
+}
+
+// TestNewServer tests whether the implementation of the transport interface is
+// accepted by the server constructor
+func TestNewServer(t *testing.T) {
+	srv, err := webwire.NewServer(
+		&TestSrvImpl{},
+		webwire.ServerOptions{
+			Host: "127.0.0.1:",
+		},
+		&wwrgorilla.Transport{},
+	)
+	require.NoError(t, err)
+	require.NotNil(t, srv)
+}
